cb38c613d506d2827afb08f027acf11e36600d20
yanhu.cheng <yanhu.cheng@binance.com>
2025-02-12 19:25:43 +0400

    update

--- handler/packet.go
+++ handler/packet.go
@@ -21,7 +21,6 @@ const (
 )
 
 type Packet struct {
-	modified     bool
 	packet       gopacket.Packet
 	packetLayers []gopacket.Layer
 }
@@ -34,7 +33,6 @@ func NewPacket(packet gopacket.Packet) (*Packet, error) {
 	packetLayers := packet.Layers()
 
 	return &Packet{
-		modified:     true,
 		packet:       packet,
 		packetLayers: packetLayers,
 	}, nil
@@ -49,7 +47,6 @@ func (p *Packet) SwapSrcDstIPv4() {
 	dst := ip.DstIP
 	ip.DstIP = ip.SrcIP
 	ip.SrcIP = dst
-	p.modified = true
 }
 
 func (p *Packet) Serialize() ([]byte, error) {
@@ -74,15 +71,3 @@ func (p *Packet) Serialize() ([]byte, error) {
 	}
 	return buf.Bytes(), nil
 }
-
-func (p *Packet) insideUDPLayerIdx() int {
-	return len(p.packetLayers) - 4
-}
-
-// - Layer 1 (14 bytes) = Ethernet	{Contents=[..14..] Payload=[..128..] SrcMAC=06:30:ef:83:93:87 DstMAC=06:36:45:f1:49:d7 EthernetType=IPv4 Length=0}
-// - Layer 2 (20 bytes) = IPv4	{Contents=[..20..] Payload=[..108..] Version=4 IHL=5 TOS=0 Length=128 Id=0 Flags= FragOffset=0 TTL=255 Protocol=UDP Checksum=41471 SrcIP=10.126.130.56 DstIP=10.126.130.57 Options=[] Padding=[]}
-// - Layer 3 (08 bytes) = UDP	{Contents=[..8..] Payload=[..100..] SrcPort=62039 DstPort=6081(geneve) Length=108 Checksum=52104}
-// - Layer 4 (40 bytes) = Geneve	{Contents=[..40..] Payload=[..60..] Version=0 OptionsLength=32 OAMPacket=false CriticalOption=false Protocol=IPv4 VNI=0 Options=[{Class=264 Type=1 Flags=0 Length=12 Data=[..8..]}, {Class=264 Type=2 Flags=0 Length=12 Data=[..8..]}, {Class=264 Type=3 Flags=0 Length=8 Data=[140, 190, 228, 157]}]}
-// - Layer 5 (20 bytes) = IPv4	{Contents=[..20..] Payload=[..40..] Version=4 IHL=5 TOS=0 Length=60 Id=22202 Flags=DF FragOffset=0 TTL=62 Protocol=TCP Checksum=27388 SrcIP=172.21.100.37 DstIP=10.0.96.203 Options=[] Padding=[]}
-// - Layer 6 (40 bytes) = TCP	{Contents=[..40..] Payload=[] SrcPort=56436 DstPort=5201(targus-getdata1) Seq=227517945 Ack=0 DataOffset=10 FIN=false SYN=true RST=false PSH=false ACK=false URG=false ECE=false CWR=false NS=false Window=62727 Checksum=19022 Urgent=0 Options=[..5..] Padding=[]}
-// 2025/02/12 14:27:45 failed to serialize packet: failed to serialize layer: TCP/IP layer 4 checksum cannot be computed without network layer... call SetNetworkLayerForChecksum to set which layer to use
