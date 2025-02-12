c4db687a7709dd16df72020455f980dcd92984eb
yanhu.cheng <yanhu.cheng@binance.com>
2025-02-12 20:49:23 +0400

    update

--- main.go
+++ main.go
@@ -5,13 +5,12 @@ import (
 	"os"
 
 	"github.com/AkihiroSuda/go-netfilter-queue"
-	"github.com/google/gopacket/layers"
 )
 
 func main() {
 	var err error
 
-	nfq, err := netfilter.NewNFQueue(0, 100, netfilter.NF_DEFAULT_PACKET_SIZE)
+	nfq, err := netfilter.NewNFQueue(0, 10000, netfilter.NF_DEFAULT_PACKET_SIZE)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -22,9 +21,9 @@ func main() {
 	for true {
 		select {
 		case p := <-packets:
-			PacketLayers := p.Packet.Layers()
-			ip := PacketLayers[3].(*layers.IPv4)
-			fmt.Println("src_ip: %s dst_ip: %s", ip.SrcIP, ip.DstIP)
+			// PacketLayers := p.Packet.Layers()
+			// ip := PacketLayers[3].(*layers.IPv4)
+			// fmt.Println("src_ip: %s dst_ip: %s", ip.SrcIP, ip.DstIP)
 			p.SetVerdict(netfilter.NF_ACCEPT)
 		}
 	}
