0cad8fa843c15c48937028f17954eead07f7f7b7
yanhu.cheng <yanhu.cheng@binance.com>
2025-02-12 22:06:41 +0400

    update

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"syscall"
 
 	"github.com/florianl/go-nfqueue"
-	"github.com/mdlayher/netlink"
 )
 
 func main() {
@@ -24,11 +23,11 @@ func main() {
 		log.Fatalf("Failed to open NFQueue: %v", err)
 	}
 	defer nfq.Close()
-	if err := nfq.SetOption(netlink.NoENOBUFS, true); err != nil {
-		log.Printf("failed to set netlink option %v: %v\n",
-			netlink.NoENOBUFS, err)
-		return
-	}
+	// if err := nfq.SetOption(netlink.NoENOBUFS, true); err != nil {
+	// 	log.Printf("failed to set netlink option %v: %v\n",
+	// 		netlink.NoENOBUFS, err)
+	// 	return
+	// }
 
 	gopacketCallback := func(a nfqueue.Attribute) int {
 		id := *a.PacketID
